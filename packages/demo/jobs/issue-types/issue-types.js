e366d1fae9687e7cc3dda7d47e999246aa84dbc8
nayakam <thillakan@gmail.com>
2016-06-15 07:27:33 +1000

    Changes

--- packages/demo/jobs/issue-types/issue-types.js
+++ packages/demo/jobs/issue-types/issue-types.js
@@ -3,63 +3,70 @@ var cheerio = require('cheerio');
 var Client = require('node-rest-client').Client;
 
 module.exports = {
-  onRun: function (config, dependencies, jobCallback) {
-    status = { "Team" : [{"email": "a@yahoo.com", "message": "Test 1", "time" :"2016-06-13T08:01:40.081Z"},
-      {"email": "b@yahoo.com", "message": " sick" , "time" :"2016-06-13T09:01:40.081Z"},
-      {"email": "c@test.com", "message": " Working from Home" , "time" :"2016-06-13T22:01:40.081Z"},
-      {"email": "d@yahoo.com", "message": "With Client" , "time" :"2016-06-13T12:20:40.081Z" }] };
+    onRun: function(config, dependencies, jobCallback) {
+        status = {
+            "Team": [{
+                "email": "Testname Surname",
+                "message": "Yahooo !!!",
+                "time": "2016-06-14 08:01:40"
+            }, {
+                "email": "Atlasboard Jira",
+                "message": " Sick",
+                "time": "2016-06-14 09:01:40"
+            }, {
+                "email": "Nodejs Javascript",
+                "message": " Working from home",
+                "time": "2016-06-14 09:01:40"
+            }, {
+                "email": "James Bond",
+                "message": "Waiting in beach side hotel :) _/\\_",
+                "time": "2016-06-14 00:20:40"
+            }]
+        };
 
 
-    var logger = dependencies.logger;
-    logger.warn('status: ' + status );
-    // logger.debug('TEST DEBUG');
-    // logger.warn('TEST WARN');
-    // logger.log('TEST LOG');
-    // logger.trace('TEST TRACE');
-    //
-    // logger.log('hello');
-    // logger.trace('hello', 'world');
-    // logger.debug('hello %s',  'world', 123);
-    // logger.info('hello %s %d',  'world', 123, {foo:'bar'});
-    // logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'});
-    // logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object);
+        var logger = dependencies.logger;
 
-    var options = {
-      url: 'https://necsparkbot.herokuapp.com/stats',
-    //  path: '/stats',
-      method: 'GET'
-    };
+        // logger.debug('TEST DEBUG');
+        // logger.warn('TEST WARN');
+        // logger.log('TEST LOG');
+        // logger.trace('TEST TRACE');
+        //
+        // logger.log('hello');
+        // logger.trace('hello', 'world');
+        // logger.debug('hello %s',  'world', 123);
+        // logger.info('hello %s %d',  'world', 123, {foo:'bar'});
+        // logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'});
+        // logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object);
 
-    dependencies.request(options, function(err, response, body) {
-          logger.info('response.statusCode : ' + response.statusCode  );
-          logger.info('response.statusMessage : ' + response.statusMessage  );
-        //  logger.warn('response.body : ' + response.body  );
-          logger.info('body : ' + body  );
-          logger.info('body : ' + body.replace(/(\r\n|\n|\r)/gm,""));
-          logger.info('body : ' + body.replace(/\n/g,""));
-          logger.info('body : ' + body.replace(/\n/g,"").replace(/\s/g,""));
-         jobCallback(null, {status: body, title: config.widgetTitle});
-    });
+        var options = {
+            url: 'https://necsparkbot.herokuapp.com/stats1',
+            //  path: '/stats',
+            method: 'GET'
+        };
 
-    //jobCallback(null, {status: status, title: config.widgetTitle});
+        dependencies.request(options, function(err, response, body) {
+            logger.info('response.statusCode : ' + response.statusCode);
+            logger.info('response.statusMessage : ' + response.statusMessage);
+            //  logger.warn('response.body : ' + response.body  );
 
-    // var client = new Client();
-    // // direct way
-    // client.get("https://necsparkbot.herokuapp.com/stats", function (data, response) {
-    //     // parsed response body as js object
-    //     logger.info(data);
-    //     // raw response
-    //     logger.info(response);
-    // });
-    //
-    // // registering remote methods
-    // client.registerMethod("jsonMethod", "https://necsparkbot.herokuapp.com/stats", "GET");
-    //
-    // client.methods.jsonMethod(function (data, response) {
-    //     // parsed response body as js object
-    //     logger.info(data);
-    //     // raw response
-    //     logger.info(response);
-    // });
-  }
+            //  logger.info('body : ' + body.replace(/(\r\n|\n|\r)/gm,""));
+            //  logger.info('body : ' + body.replace(/\n/g,""));
+            //  logger.info('body : ' + body.replace(/\n/g,"").replace(/\s/g,""));
+            if (response.statusCode === 200) {
+                logger.info('body : ' + body);
+                jobCallback(null, {
+                    status: body,
+                    title: config.widgetTitle
+                });
+            } else {
+                logger.warn('body : ' + body);
+                logger.info('Dummy JSON Object status : ' + JSON.stringify(status));
+                jobCallback(null, {
+                    status: JSON.stringify(status),
+                    title: config.widgetTitle
+                });
+            }
+        });
+    }
 };
--- packages/demo/widgets/keyvaluelist/keyvaluelist.js
+++ packages/demo/widgets/keyvaluelist/keyvaluelist.js
@@ -1,73 +1,73 @@
 widget = {
 
-  onData: function (el, data) {
-    if (data.title) {
-      $('h2', el).text(data.title);
+    onData: function(el, data) {
+        if (data.title) {
+            $('h2', el).text(data.title);
+        }
+
+        var $content = $('.content', el);
+        $content.empty();
+        console.log("data:" + data);
+        console.log("data.status:" + data.status);
+        console.log("data.status.length :" + data.status.length);
+
+        if (data.status.Team != null) {
+            console.log("<div> data.status.Team : " + data.status.Team);
+            console.log("<div> data.status.Team.length : " + data.status.Team.length);
+            if (data.status.Team.length > 0) {
+                console.log(" data.status.Team[0] : " + data.status.Team[0]);
+            }
+        } else {
+            console.log("<div> data.status.Team  is " + data.status.Team);
+        }
+
+        var json = JSON.parse(data.status);
+        console.log("json : " + json);
+        if (json.Team != null) {
+            console.log(" json.Team : " + json.Team);
+            console.log(" json.Team.length : " + json.Team.length);
+
+        } else {
+            console.log("<div> json.Team is " + json.Team);
+        }
+        var table = '<table width="100%">';
+        //table += "<table class='item-container'>";
+        table += "<tr> <th width=\"35%\"> Name </th><th width=\"25%\">  Time </th> <th width=\"40%\"> Status </th></tr>";
+        if (json.Team.length) {
+            json.Team.forEach(function(team) {
+                table += "<tr>" +
+                    "<td>" + team.email + "</td>" +
+                    "<td>" + team.time + "</td>" +
+                    "<td>" + team.message + "</td>" +
+                    "</tr>";
+            })
+        }
+        table += "</table>";
+        $content.append(table);
+
+
+        // if (json.Team.length) {
+        //     json.Team.forEach(function (team) {
+        //     $content.append(
+        //         "<div class='item-container'>" +
+        //           "<div class='email'>" + team.email + "</div>" +
+        //           "<div class='time'>" + team.time + "</div>" +
+        //           "<div class='message'>" + team.message + "</div>" +
+        //         "</div>"
+        //     );
+        //   })
+        // }
+
+        // if (data.status.Team.length) {
+        //   data.status.Team.forEach(function (team) {
+        //     $content.append(
+        //         "<div class='item-container'>" +
+        //           "<div class='email'>" + team.email + "</div>" +
+        //           "<div class='time'>" + team.time + "</div>" +
+        //           "<div class='message'>" + team.message + "</div>" +
+        //         "</div>"
+        //     );
+        //   })
+        // }
     }
-
-    var $content = $('.content', el);
-    $content.empty();
-    console.log("data:" +data);
-    console.log("data.status:" +data.status);
-    console.log("data.status.length :" +data.status.length );
-
-    if(data.status.Team != null){
-      console.log(  "<div> data.status.Team : " + data.status.Team );
-      console.log(  "<div> data.status.Team.length : " + data.status.Team.length );
-      if(data.status.Team.length> 0){
-        console.log(" data.status.Team[0] : " + data.status.Team[0] );
-      }
-    }else{
-      console.log(  "<div> data.status.Team  is " + data.status.Team );
-    }
-
-    var json = JSON.parse(data.status);
-    console.log(  "json : " + json );
-    if(json.Team != null){
-      console.log(" json.Team : " + json.Team );
-      console.log(" json.Team.length : " + json.Team.length );
-
-    }else{
-        console.log("<div> json.Team is " + json.Team );
-    }
-    var table = '<table width="100%">';
-    //table += "<table class='item-container'>";
-    table += "<tr> <th width=\"35%\"> Name </th><th width=\"25%\">  Time </th> <th width=\"40%\"> Status </th></tr>";
-    if (json.Team.length) {
-      json.Team.forEach(function (team) {
-          table += "<tr>" +
-              "<td>" + team.email + "</td>" +
-              "<td>" + team.time + "</td>" +
-              "<td>" + team.message + "</td>" +
-            "</tr>";
-      })
-    }
-    table +="</table>";
-    $content.append(table);
-
-
-    // if (json.Team.length) {
-    //     json.Team.forEach(function (team) {
-    //     $content.append(
-    //         "<div class='item-container'>" +
-    //           "<div class='email'>" + team.email + "</div>" +
-    //           "<div class='time'>" + team.time + "</div>" +
-    //           "<div class='message'>" + team.message + "</div>" +
-    //         "</div>"
-    //     );
-    //   })
-    // }
-
-    // if (data.status.Team.length) {
-    //   data.status.Team.forEach(function (team) {
-    //     $content.append(
-    //         "<div class='item-container'>" +
-    //           "<div class='email'>" + team.email + "</div>" +
-    //           "<div class='time'>" + team.time + "</div>" +
-    //           "<div class='message'>" + team.message + "</div>" +
-    //         "</div>"
-    //     );
-    //   })
-    // }
-  }
 };
